575b87f24030b4e1a76f0181cbf19d2a8f19f936
cpwestlund <cpwestlund@gmail.com>
2026-09-05 22:09:58 +0200

    V32 show new zoom controls in HUD

--- unity/Assets/Rosvik/Scripts/RosvikMapHUD.cs
+++ unity/Assets/Rosvik/Scripts/RosvikMapHUD.cs
@@ -58,8 +58,8 @@ namespace Rosvik.Blackout {
                 GUI.Label(rect, p.name, titleStyle);
             }
 
-            Rect hint = new Rect(16f, Screen.height - 42f, 560f, 26f);
-            GUI.Label(hint, "WASD flyttar • Shift springer • E interagerar • mushjul zoomar", labelStyle);
+            Rect hint = new Rect(16f, Screen.height - 42f, 690f, 26f);
+            GUI.Label(hint, "WASD flyttar • Shift springer • E interagerar • mushjul / +/- zoomar • Home återställer zoom", labelStyle);
         }
     }
 }
